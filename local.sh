4f73f57f7b31eed41722d58e41e452e9252058e6
gjbae1212 <gjbae1212@gmail.com>
2020-02-17 18:31:15 +0900

    Add deploy script.

--- local.sh
+++ local.sh
@@ -11,6 +11,22 @@ function test
    go test -v $(go list ./... | grep -v vendor) --count 1 -covermode=atomic --race -timeout 120s
 }
 
+function release
+{
+   phase=$1
+   case "$phase" in
+   test)
+     goreleaser release --snapshot --skip-publish
+     ;;
+   real)
+     goreleaser release --rm-dist
+     ;;
+   *)
+     echo "unsupported phase=$phase"
+     exit 1
+   esac
+}
+
 function set_env
 {
    source $CURRENT/local_env.sh
